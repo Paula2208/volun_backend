f97345e026b2dbd376c819dd4de396314b1457ef
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-11-10 16:19:07 -0500

    routes-forgot-password

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 
 const {
-    changePassword,
+    forgotPassword,
     createUser,
     sendCodeNumber,
+    sendMail,
     logIn
 } = require('../entities/auth')
 
@@ -13,10 +14,11 @@ router.route('/auth')
 router.route('/auth/user')
       .post(createUser);
 
-router.route('/auth/forgotPassword')
-      .get(sendCodeNumber);
 
+router.route('/auth/forgotPassword')
+      .post(forgotPassword);
+/*
 router.route('/auth/forgotPassword')
       .post(changePassword);
-
+*/
 module.exports = router;

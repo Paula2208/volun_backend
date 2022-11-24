6ac870e1ae07783768c6e44b33a42b1af7262e1e
XdmriveroeX <104804272+XdmriveroeX@users.noreply.github.com>
2022-11-24 11:39:26 -0500

    reports api

--- src/routes/reports.js
+++ src/routes/reports.js
@@ -1,12 +1,17 @@
 const router = require('express').Router();
 const {
-    functionTemplate
+    functionTemplate, postReportsActive, postReportsPending, postReportsDenied
 } = require('../entities/reports')
 
 router.route('/')
       .get(functionTemplate);
 
-router.route('/postReports')
-      .get(functionTemplate);
+router.route('/postReportsActive')
+      .get(postReportsActive);
 
+router.route('/postReportsPending')
+      .get(postReportsPending);  
+      
+      router.route('/postReportsDenied')
+      .get(postReportsDenied);
 module.exports = router;

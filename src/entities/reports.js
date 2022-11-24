ef5ff427533003d3fa54413f3c0d62fd325ef818
Dacosta99 <dyaluzan@unal.edu.co>
2022-11-24 13:01:21 -0500

    consulta actualizada endpoint get volunters report per non profit

--- src/entities/reports.js
+++ src/entities/reports.js
@@ -14,9 +14,8 @@ const voluteersReport = async(req, res) => {
     const PENDIN = 'PENDIN';
     const ACTIVE = 'ACTIVE';
     const DENIED = 'DENIED';
-    const id = req.params.id;
     const rows = await pool.query(' select o.nonProfitUsername as nonProfitUsername, o.nonProfitName as nonProfitName, count(if(a.applicationStatus =? and o.id=a.id,1,NULL)) as pending, count(if(a.applicationStatus =? and o.id=a.id,1,NULL)) as active, count(if(a.applicationStatus =? and o.id=a.id,1,NULL)) as denied from Ofertas as o, Aplican as a group by o.id',
-    [PENDIN, ACTIVE, DENIED, id]);
+    [PENDIN, ACTIVE, DENIED]);
     if(rows.length>0){
       res.send(rows); 
     }
